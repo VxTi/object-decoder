07db5d54f86d53ff352cab5085ff2d0dbdabe77f
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2025-12-25 13:58:18 +0100

    1.0.11

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import * as patterns from './common/patterns.js';
-import * as array from './core/array.js';
-import * as boolean from './core/boolean.js';
-import * as enumerable from './core/enum.js';
-import * as literal from './core/literal.js';
-import * as number from './core/number.js';
-import * as object from './core/object.js';
-import * as optional from './core/optional.js';
-import * as record from './core/record.js';
-import * as string from './core/string.js';
-import * as union from './core/union.js';
+import * as arrays from './core/array.js';
+import * as bools from './core/boolean.js';
+import * as enumerables from './core/enum.js';
+import * as literals from './core/literal.js';
+import * as numbers from './core/number.js';
+import * as objects from './core/object.js';
+import * as optionals from './core/optional.js';
+import * as records from './core/record.js';
+import * as strings from './core/string.js';
+import * as unions from './core/union.js';
 
 export * from './common/index.js';
 
 export const $ = {
-  ...string,
-  ...number,
-  ...boolean,
-  ...array,
-  ...object,
-  ...record,
-  ...literal,
-  ...enumerable,
-  ...optional,
-  ...union,
+  ...strings,
+  ...numbers,
+  ...bools,
+  ...arrays,
+  ...objects,
+  ...records,
+  ...literals,
+  ...enumerables,
+  ...optionals,
+  ...unions,
   patterns,
 };
